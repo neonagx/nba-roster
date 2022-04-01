8780d28be8b9f225f1b5271e61e00f2d391d53ec
Sukhyun Baik <sukhyun.baik@leidos.com>
2022-04-01 14:21:16 -0400

    have cards align and have player info

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import NbaGrid from './Nbagrid';
 import './App.css';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Avatar from '@mui/material/Avatar';
 
 const App = () => {
   const [players, setPlayers] = useState<any[]>([]);
@@ -13,6 +15,7 @@ const App = () => {
       "http://data.nba.net/10s/prod/v1/2021/players.json")
       .then((res) => res.json())
       .then((data) => {
+        console.log('data', data.league.standard[0])
         setPlayers(data.league.standard);
       })
   }, [])
@@ -20,16 +23,22 @@ const App = () => {
   return (
     <div className="App">
       {players.map((player) => (
-        <Card className={"playerCard"} sx={{ maxWidth: 345 }} key={player.personId}>
-          {/* <CardHeader avatar={
-
-        }/> */}
+        <Card className={"playerCard"} sx={{ width: '100%',maxWidth: 345, display: 'inline-block'}} key={player.personId}>
+          <CardHeader
+          title={`${player.firstName} ${player.lastName}`}
+          />
           <CardMedia
             component="img"
             height="194"
             image={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.personId}.png`}
             alt="Player Image Not Available"
           />
+          <CardContent>
+            PLAYER POSITION: 
+          <Avatar alt={'position'} style={{display: 'inline-flex', marginLeft: '10px'}}>
+              {player.pos}
+          </Avatar>
+          </CardContent>
         </Card>
       ))}
 
