c93a77a4175431ea608ffea2dca2f1b95e3db8c9
neonagx <neonagx@gmail.com>
2022-04-01 08:49:26 -0400

    create avatar media card and have headshots rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NbaGrid from './Nbagrid';
 import './App.css';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
 
-function App() {
+const App = () => {
+  const [players, setPlayers] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "http://data.nba.net/10s/prod/v1/2021/players.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setPlayers(data.league.standard);
+      })
+  }, [])
+
   return (
     <div className="App">
-     <NbaGrid />
+      {players.map((player) => (
+        <Card className={"playerCard"} sx={{ maxWidth: 345 }} key={player.personId}>
+          {/* <CardHeader avatar={
+
+        }/> */}
+          <CardMedia
+            component="img"
+            height="194"
+            image={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.personId}.png`}
+            alt="Player Image Not Available"
+          />
+        </Card>
+      ))}
+
+      <NbaGrid />
     </div>
   );
 }
